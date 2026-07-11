74f5e9437940c700a41e6022cc0e7c57bf63fa35
Vedant <vedantmakwana591@gmail.com>
2026-07-11 15:14:17 +0530

    demo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 let port = 9000;
+let cors = require('cors');
+app.use(cors());
+
 app.get('/ping', (req,res) => {
     res.send({
-        message: 'Hello, World!'
+        message: 'Hello i am vedant'
     });
 });
 app.listen(port, () => {
     console.log('Server is running on http://localhost:9000');
 });
+ 
